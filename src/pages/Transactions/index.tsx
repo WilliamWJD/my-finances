5923e6bd59d397d4b894510440b0703a9fbab450
WilliamWJD <william.jose.wjd@gmail.com>
2023-05-01 20:30:18 -0300

    feat: criando table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,55 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHeighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
-export function Transactions(){
-    return(
-        <div>
-            <Header/>
-            <Summary/>
-        </div>
-    )
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHeighLight variant="income">R$ 12.000,00</PriceHeighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Amburguer</td>
+              <td>
+                <PriceHeighLight variant="outcome">R$ 50,00</PriceHeighLight>
+              </td>
+              <td>FastFood</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Consultoria</td>
+              <td>
+                <PriceHeighLight variant="income">R$ 1.250,00</PriceHeighLight>
+              </td>
+              <td>Trabalho</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Parcela da faculdade</td>
+              <td>
+                <PriceHeighLight variant="outcome">R$ 450,00</PriceHeighLight>
+              </td>
+              <td>Educação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
